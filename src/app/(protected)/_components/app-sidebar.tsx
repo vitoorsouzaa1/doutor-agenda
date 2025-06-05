c62ed3e0e509e674398d116a781e7f336da0a830
Vitor Souza <vitor.souza407@yahoo.com.br>
2025-06-05 12:09:56 -0300

    feat: create page container components

--- src/app/(protected)/_components/app-sidebar.tsx
+++ src/app/(protected)/_components/app-sidebar.tsx
@@ -30,7 +30,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 // Menu items.
@@ -60,6 +60,7 @@ const items = [
 export function AppSidebar() {
   const router = useRouter();
   const session = authClient.useSession();
+  const pathname = usePathname();
 
   const handleSignOut = async () => {
     await authClient.signOut({
@@ -83,7 +84,7 @@ export function AppSidebar() {
             <SidebarMenu>
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
                     <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
---
+++ src/app/(protected)/doctors/page.tsx
@@ -0,0 +1,35 @@
+import {
+  PageContainer,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+  PageActions,
+  PageContent,
+} from "@/components/ui/page-container";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+
+const DoctorsPage = () => {
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica.</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <h1>Médicos</h1>
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default DoctorsPage;
