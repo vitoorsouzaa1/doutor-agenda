27bf2967b356fac600bfd0b8a514389de6f0cb1b
Vitor Souza <vitor.souza407@yahoo.com.br>
2025-06-06 18:36:53 -0300

    feat: fetch and display doctors for the user's clinic on the doctors page

--- src/app/(protected)/doctors/page.tsx
+++ src/app/(protected)/doctors/page.tsx
@@ -1,44 +1,53 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import {
+  PageActions,
   PageContainer,
+  PageContent,
   PageDescription,
   PageHeader,
   PageHeaderContent,
   PageTitle,
-  PageActions,
-  PageContent,
 } from "@/components/ui/page-container";
+import { db } from "@/src/db";
+import { doctorsTable } from "@/src/db/schema";
+import { auth } from "@/lib/auth";
 
 import AddDoctorButton from "./_components/add-doctor-button";
-
-import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
-import { headers } from "next/headers";
+import DoctorCard from "./_components/doctor-card";
 
 const DoctorsPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
   if (!session?.user) {
     redirect("/authentication");
   }
   if (!session.user.clinic) {
     redirect("/clinic-form");
   }
-
+  const doctors = await db.query.doctorsTable.findMany({
+    where: eq(doctorsTable.clinicId, session.user.clinic.id),
+  });
   return (
     <PageContainer>
       <PageHeader>
         <PageHeaderContent>
           <PageTitle>Médicos</PageTitle>
-          <PageDescription>Gerencie os médicos da sua clínica.</PageDescription>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
         </PageHeaderContent>
         <PageActions>
           <AddDoctorButton />
         </PageActions>
       </PageHeader>
       <PageContent>
-        <h1>Médicos</h1>
+        <div className="grid grid-cols-3 gap-6">
+          {doctors.map((doctor) => (
+            <DoctorCard key={doctor.id} doctor={doctor} />
+          ))}
+        </div>
       </PageContent>
     </PageContainer>
   );
